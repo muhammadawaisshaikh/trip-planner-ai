5d673ce09d91bdbfda0b546f56b8fbbff1699f16
Muhammad Awais <muhammad.awais@royalcyber.com>
2024-05-17 21:55:05 +0500

    search filter with banner done along with mobile responsive

--- src/app/components/search-filter/search-filter.component.ts
+++ src/app/components/search-filter/search-filter.component.ts
@@ -1,12 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-search-filter',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './search-filter.component.html',
   styleUrl: './search-filter.component.scss'
 })
-export class SearchFilterComponent {
+export class SearchFilterComponent implements OnInit {
+  tripForm!: FormGroup;
 
+  constructor(
+    private fb: FormBuilder
+  ) { }
+
+  ngOnInit(): void {
+    this.formInit();
+  }
+
+  formInit() {
+    this.tripForm = this.fb.group({
+      countryToVisit: [''],
+      countryComingFrom: [''],
+      dateFrom: [''],
+      dateTo: [''],
+      budget: [''],
+      hotel: [false],
+      flight: [false],
+      taxi: [false]
+    });
+  }
+
+  planTrip() {
+    console.log('Trip planned with data:', this.tripForm.value);
+  }
 }
